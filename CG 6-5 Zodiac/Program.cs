7902a044dc2634c5f0005eb877d8022fbca85c83
Kristen Shea <kstar61@live.com>
2018-08-15 17:31:14 -0500

    started zodiac app

--- CG 6-5 Zodiac/Program.cs
+++ CG 6-5 Zodiac/Program.cs
@@ -6,7 +6,23 @@ namespace CG_6_5_Zodiac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*Create a program that asks the user for the date they were born.
+             * Display the name of the day they were born on(Ex: Thursday) and the name of the day their next birthday will be on.
+             * Finally, display their zodiac symbol and a fake fortune for them.You can get the list of zodiac signs and dates, 
+             * as well as a sample, from Bing to an external site.*/
+
+            Console.WriteLine("What is your sign and horoscope?");
+            Console.WriteLine();
+            Console.Write("Please enter your birthdate (MM/DD/YYYY): ");
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
