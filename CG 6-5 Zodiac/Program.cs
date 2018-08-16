cce42ae1b82e54d79b9a99701fc62740b70039d5
Kristen Shea <kstar61@live.com>
2018-08-15 23:18:12 -0500

    everything but the fortunes

--- CG 6-5 Zodiac/Program.cs
+++ CG 6-5 Zodiac/Program.cs
@@ -7,42 +7,48 @@ namespace CG_6_5_Zodiac
         static void Main(string[] args)
         {
 
-            /*Create a program that asks the user for the date they were born.
-             * Display the name of the day they were born on(Ex: Thursday) and the name of the day their next birthday will be on.
+            /*
              * Finally, display their zodiac symbol and a fake fortune for them.You can get the list of zodiac signs and dates, 
              * as well as a sample, from Bing to an external site.*/
 
             Console.WriteLine("What is your Zodiac sign?");
-            Console.WriteLine();
+            Console.WriteLine(); //line break for ease of reading
+
+            //prompt for date of birth
             Console.Write("Please enter your date of birth (MM/DD/YYYY): ");
 
-
+            //declare as datetime and name variable, and then convert from string to datetime using parse
             DateTime userBirthdate = DateTime.Parse(Console.ReadLine());
             
-            Console.WriteLine("You were born on {0}.", userBirthdate.ToString("dddd"));
+            //determine day of week birthdate is using the ToString method and print it to console
+            Console.WriteLine("You were born on a {0}.", userBirthdate.ToString("dddd"));
 
             DateTime currentdate = DateTime.Now;
+            //assign variable name and type to current date
 
-            DateTime nextBDay = 
-            userBirthdate.Month
-            userBirthdate.Day
-            currentdate.Year
-            
+            DateTime nextBDay = new DateTime(currentdate.Year, userBirthdate.Month, userBirthdate.Day);
+            //create new datetime variable for birthday in current year to use in if/else if statements below
 
-            //DateTime nextyearBDay = userBirthdate.AddYears();
-
-
-            //Console.WriteLine("Your next birthday will be on {0}", );
-            
-
-
-            string zodiacDay = userBirthdate.ToString("dd");
-            string zodiacMonth = userBirthdate.ToString("MMM");
+            //if day of year of your birthday is greater than or equal to current day of year,
+            //the day of week will be determined by using current year
+            //print day of the week to console using ToString
+            if (userBirthdate.DayOfYear >= currentdate.DayOfYear)
+            {
+                Console.WriteLine("Your next birthday will be on a {0}.", nextBDay.ToString("dddd"));
+            }
 
 
+            //if day of year of your birthday has happened already this year, add 1 year to date to get correct day
+            //print to console using ToString
+            else if (userBirthdate.DayOfYear < currentdate.DayOfYear)
+            {
+                DateTime nextBDdayofwk = nextBDay.AddYears(1);
+                Console.WriteLine("Your next birthday will be on a {0}", nextBDdayofwk.ToString("dddd"));
+            }
 
 
-            //string sign = 
+
+
 
             //if (sign = "Aries")
             //    Console.WriteLine("Fortune: You will reconnect with an old friend this month.");
@@ -51,10 +57,12 @@ namespace CG_6_5_Zodiac
             //else if (sign = "Gemini")
             //    Console.WriteLine("Fortune: You will find a worm in the next apple you eat.");
             //else if (sign = "Cancer")
-                
-            
-          
 
+
+
+            
+
+            
 
 
 
@@ -64,22 +72,26 @@ namespace CG_6_5_Zodiac
             Console.ReadLine();
         }
 
-        //private static string ZodiacSign(string zodiacMonth, string zodiacDay);
+        //private static string Zodiac(DateTime userBirthdate)
         //{
-           
             
-        
-        //// if (string zodiacMonth = "Mar" && string zodiacDay >= 21 || string zodiacMonth = "Apr" && string zodiacDay <= 19 )
-        //   //{
-        //   //     return = "Aries";
 
-        //   //}
+        //    //string birthMonth = userBirthdate.ToString("MM");
+        //    //string birthDay = userBirthdate.ToString("dd");
+        //    //int birthMonthInt = int.Parse(birthMonth);
+        //    //int birthDayInt = int.Parse(birthDay);
 
+        //    if ()
+        //    {
 
+        //    }
 
-
+            
 
         //}
+
+
+
 
 
 
